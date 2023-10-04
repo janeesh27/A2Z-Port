f7d2e6eb18481649f7c73fabedfadf89fba3c094
janeesh27 <janeesh.tin@gmail.com>
2023-10-04 16:30:26 +0530

    more efficent method

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-function findLongestWord(sentence) {
-  function countVowels(word) {
-    return (word.match(/[aeiouAEIOU]/g) || []).length;
-  }
+const getLongestWord = (str) => {
+  const words = str.split(/\s+/);
 
-  let longestWord = "";
-  let maxLength = 0;
-  let maxVowels = 0;
+  let maxLength = "";
+  let maxVowelCount = 0;
 
-  const words = sentence.match(/[a-zA-Z]+/g);
+  words.forEach((word) => {
+    const cleanWord = word.replace(/[^\w ]/g, "");
+    const vowelCount = (cleanWord.match(/[aeiouAEIOU]/g) || []).length;
 
-  if (words) {
-    words.forEach((word) => {
-      const length = word.length;
-      const vowels = countVowels(word);
+    if (cleanWord.length > maxLength.length) {
+      maxLength = cleanWord;
+      maxVowelCount = vowelCount;
+    } else if (
+      cleanWord.length === maxLength.length &&
+      vowelCount > maxVowelCount
+    ) {
+      maxLength = cleanWord;
+      maxVowelCount = vowelCount;
+    }
+  });
 
-      if (length > maxLength || (length === maxLength && vowels > maxVowels)) {
-        longestWord = word;
-        maxLength = length;
-        maxVowels = vowels;
-      }
-    });
-  }
-
-  return longestWord;
-}
+  return maxLength;
+};
 
 const sentence =
-  "Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers";
+  "Smart people learn from everything and everyone, average people from their experience, stupid people already have all the answers";
 
-const longestWord = findLongestWord(sentence);
-console.log(longestWord);
+console.log(getLongestWord(sentence));
